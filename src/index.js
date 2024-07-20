6f5d845c0ebce42cde37c4af21c96775872fc4ef
seandrury <sean_drury@hotmail.co.uk>
2024-07-20 12:36:09 +0100

    add checkboxes and switches

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import _ from 'lodash';
 
-function component() {
-    const element = document.createElement('div');
+// function component() {
+//     const element = document.createElement('div');
   
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'computer'], ' ');
+//     // Lodash, currently included via a script, is required for this line to work
+//     element.innerHTML = _.join(['Hello', 'computer'], ' ');
   
-    return element;
-  }
+//     return element;
+//   }
   
-  document.body.appendChild(component());
+//   document.body.appendChild(component());
+
+function toggleColourMode() {
+	$(".toggle-colours .checkbox").change(function() {
+		if(this.checked) {
+			$(".interactive-map").removeClass('team-colours');
+			$(".interactive-map").addClass('league-colours');
+		}
+	});
+}
+
+$(function() {
+  	if ($('.interactive-map').length > 0) {
+		toggleColourMode();
+
+		toggleAccessibilityMode():
+  	}
+})
