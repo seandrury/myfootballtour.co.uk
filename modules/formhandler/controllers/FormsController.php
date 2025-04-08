3d6722729012998f86f54bb577f5b0d1e84ad672
seandrury <sean_drury@hotmail.co.uk>
2025-04-08 20:50:27 +0100

    more module

--- modules/formhandler/controllers/FormsController.php
+++ modules/formhandler/controllers/FormsController.php
@@ -5,12 +5,12 @@ namespace modules\formhandler\controllers;
 use Craft;
 use craft\web\Controller;
 use yii\web\Response;
-
+use craft\helpers\UrlHelper;
 use modules\formhandler\Module;
 
 class FormsController extends Controller
 {
-    public bool|int $allowAnonymous = false;
+    protected array|int|bool $allowAnonymous = true;
 
     public function actionSendForm(): Response
     {
@@ -20,15 +20,15 @@ class FormsController extends Controller
         $formData = Craft::$app->getRequest()->getBodyParams();
 
         // Call the module's method to send the email
-        $module = Craft::$app->getModule('myformmodule');
+        $module = Craft::$app->getModule('formhandler');
         if ($module && $module->sendFormEmail($formData)) {
             // Redirect or display a success message
             Craft::$app->getSession()->setFlash('success', 'Form submitted successfully!');
-            return $this->redirect(UrlHelper::siteUrl('thank-you'));
+            return $this->redirect(UrlHelper::siteUrl('contact?send=success'));
         }
 
         // Handle failure
         Craft::$app->getSession()->setFlash('error', 'There was an issue with your submission.');
-        return $this->redirect(UrlHelper::siteUrl('contact-us'));
+        return $this->redirect(UrlHelper::siteUrl('contact'));
     }
 }
